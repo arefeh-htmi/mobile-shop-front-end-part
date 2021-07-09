1cd07491738833020d9b94dc4f23b954c084724b
arefeh_htmi <hatamiarefeh.ah@gmail.com>
2021-07-09 15:55:54 +0200

    ffiles added

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "@styles/Footer.css";
+
+const Footer = () => (
+  <div className="footer">
+    <p>MobileShop using MongoDB, ExprssJS, NodeJS, React</p>
+    <p>Made by arefeh, mohammad and Shayan; 2019 </p>
+  </div>
+);
+
+export default Footer;
